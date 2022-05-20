13e569f368ffb02236eb1bef7727c9058840fe47
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-05-19 22:40:00 -0500

    funciona xfa

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,59 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Object>{
+    private String nombre;
+    private int edad;
+    private String posicion;
+	public Futbolista(String nombre, int edad, String pos) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = pos;
+	}
+	public Futbolista() {
+		this.nombre="Maradona";
+		this.edad=30;
+		this.posicion="delantero";
+	}
+    public String toString() {
+    	return "El futbolista "+nombre+" tiene "+edad+" y juega de "+posicion;
+    }
+    public boolean equals(Futbolista f) {
+    	if (this.nombre.equals(f.nombre)) {
+    		if (this.posicion.equals(f.posicion)) {
+    			if (this.edad==f.edad) {
+    				return(true);
+    			}
+    			else {
+    				return false;
+    			}
+    		}
+    		else {
+    			return false;
+    		}
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    public abstract boolean jugarConLasManos();
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+    
+}
